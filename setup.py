54a512c5fdcbed2d14904037d2f10d4f3f6cc9ef
Thiago Pisani <pisani.thiago@gmail.com>
2014-06-22 15:13:41 -0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-inspect",
+    version="0.1dev",
+    packages=find_packages(),
+    url="https://github.com/tpisani/django-inspect",
+)
