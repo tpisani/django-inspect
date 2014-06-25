20ee2d8fe07121b14d0c94e1605a7ce9dc593118
Thiago Pisani <pisani.thiago@gmail.com>
2014-06-25 12:24:19 -0300

    better tests - ensuring to keep field ordering

--- django_inspect/tests/test_inspect.py
+++ django_inspect/tests/test_inspect.py
@@ -16,13 +16,33 @@ class InspectTests(TestCase):
         self.assertEqual(self.inspect.fields, ["id",
                                                "int",
                                                "char",
-                                               "text",
-                                               "direct_fk"])
+                                               "text"])
+
+    def test_local_fields(self):
+        self.assertEqual(self.inspect.local_fields, ["id",
+                                                     "int",
+                                                     "char",
+                                                     "text",
+                                                     "direct_fk",
+                                                     "many_to_many"])
 
     def test_fk_fields(self):
-        self.assertEqual(self.inspect.fk_fields, ["anotherfk_set",
-                                                  "direct_fk",
-                                                  "fk_set"])
+        self.assertEqual(self.inspect.fk_fields, ["direct_fk"])
+
+    def test_backwards_fk_fields(self):
+        self.assertEqual(self.inspect.backwards_fk_fields, ["fk_set",
+                                                            "anotherfk_set"])
+
+    def test_all_fk_fields(self):
+        self.assertEqual(self.inspect.all_fk_fields, ["fk_set",
+                                                      "anotherfk_set"])
 
     def test_m2m_fields(self):
-        self.assertEqual(self.inspect.m2m_fields, ["manytomany_set"])
+        self.assertEqual(self.inspect.m2m_fields, ["many_to_many"])
+
+    def test_backwards_m2m_fields(self):
+        self.assertEqual(self.inspect.backwards_m2m_fields, ["manytomany_set"])
+
+    def test_all_m2m_fields(self):
+        self.assertEqual(self.inspect.all_m2m_fields, ["many_to_many",
+                                                       "manytomany_set"])
