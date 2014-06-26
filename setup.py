745bb7a9bbb982bf107210a64e6ffa1a18fb3fd8
Thiago Pisani <pisani.thiago@gmail.com>
2014-06-25 23:24:13 -0300

    added MANIFEST, README and more information on setup

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(
     name="django-inspect",
     version="0.1dev",
     packages=find_packages(),
+    author="Thiago Pisani",
+    author_email="pisani.thiago@gmail.com",
     url="https://github.com/tpisani/django-inspect",
 )
