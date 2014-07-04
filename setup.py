cb660db582cd424ee805d529a33ff105c0e918a1
Thiago Pisani <pisani.thiago@gmail.com>
2014-07-04 14:05:40 -0300

    a little better description on setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-inspect",
-    description="Provides information about django models",
+    description="Provides information about django models by a series of conveniences",
     version="0.2",
     packages=find_packages(),
     author="Thiago Pisani",
